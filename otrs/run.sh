89807fe78bb048368eb8bb69fb4e830fa7bfc8ae
Robert Habermann <mail@rhab.de>
2016-04-13 17:24:52 +0000

    add enabling the OTRS WebService REST
    In OTRS it is possible to activate Web Services for HTTP:SOAP and for
    HTTP:REST. After the activation of a the HTTP:REST Web Services it is
    possible to use an API for interacting with Tickets.
    
    This commit adds the .yml configuration file from the HTTP:REST Web
    Service taken from the official OTRS Github Repo at:
    https://github.com/OTRS/otrs/tree/master/development/webservices
    
    After the file is added it is activated using the OTRS Console command:
    /opt/otrs/bin/otrs.Console.pl Admin::WebService::Add --name ...

--- otrs/run.sh
+++ otrs/run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-# Startup script for this OTRS container. 
+# Startup script for this OTRS container.
 #
-# The script by default loads a fresh OTRS install ready to be customized through 
-# the admin web interface. 
+# The script by default loads a fresh OTRS install ready to be customized through
+# the admin web interface.
 #
-# If the environment variable OTRS_INSTALL is set to yes, then the default web 
+# If the environment variable OTRS_INSTALL is set to yes, then the default web
 # installer can be run from localhost/otrs/installer.pl.
 #
-# If the environment variable OTRS_INSTALL="restore", then the configuration backup 
-# files will be loaded from ${OTRS_ROOT}/backups. This means you need to build 
-# the image with the backup files (sql and Confg.pm) you want to use, or, mount a 
+# If the environment variable OTRS_INSTALL="restore", then the configuration backup
+# files will be loaded from ${OTRS_ROOT}/backups. This means you need to build
+# the image with the backup files (sql and Confg.pm) you want to use, or, mount a
 # host volume to map where you store the backup files to ${OTRS_ROOT}/backups.
 #
-# To change the default database and admin interface user passwords you can define 
+# To change the default database and admin interface user passwords you can define
 # the following env vars too:
 # - OTRS_DB_PASSWORD to set the database password
-# - OTRS_ROOT_PASSWORD to set the admin user 'root@localhost' password. 
+# - OTRS_ROOT_PASSWORD to set the admin user 'root@localhost' password.
 #
 
 . ./functions.sh
@@ -43,6 +43,7 @@ if [ "$OTRS_INSTALL" != "yes" ]; then
       echo -e "Setting password for default admin account root@localhost..."
       #${OTRS_ROOT}bin/otrs.SetPassword.pl --agent root@localhost $OTRS_ROOT_PASSWORD
       su -c "${OTRS_ROOT}bin/otrs.Console.pl Admin::User::SetPassword root@localhost $OTRS_ROOT_PASSWORD" -s /bin/bash otrs
+      su -c "${OTRS_ROOT}bin/otrs.Console.pl Admin::WebService::Add --name GenericTicketConnectorREST --source-path /GenericTicketConnectorREST.yml" -s /bin/bash otrs
     fi
   # If OTRS_INSTALL == restore, load the backup files in ${OTRS_ROOT}/backups
   elif [ "$OTRS_INSTALL" == "restore" ];then
@@ -51,14 +52,14 @@ if [ "$OTRS_INSTALL" != "yes" ]; then
   fi
   set_skins
   set_ticker_counter
-  set_default_language  
+  set_default_language
   rm -fr ${OTRS_ROOT}var/tmp/firsttime
   #Start OTRS
   ${OTRS_ROOT}bin/otrs.SetPermissions.pl --otrs-user=otrs --web-group=apache /opt/otrs
   ${OTRS_ROOT}bin/Cron.sh start otrs
   su -c "${OTRS_ROOT}bin/otrs.Daemon.pl start" -s /bin/bash otrs
   #/usr/bin/perl ${OTRS_ROOT}bin/otrs.Scheduler.pl -w 1
-  set_fetch_email_time  
+  set_fetch_email_time
   #${OTRS_ROOT}bin/otrs.RebuildConfig.pl
   su -c "${OTRS_ROOT}bin/otrs.Console.pl Maint::Config::Rebuild" -s /bin/bash otrs
   #${OTRS_ROOT}bin/otrs.DeleteCache.pl
